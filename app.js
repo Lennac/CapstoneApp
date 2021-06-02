c8d8b3e5e9dca61e1a5a507c8d1486283f31b63a
Andrew Cannell <and06571@tjx.com>
2021-06-02 14:22:54 -0400

    Add products router to app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express();
 const port = 3000;
 
 const indexRouter = require('./routes/index');
+const productsRouter = require('./routes/products');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
+app.use('/products', productsRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on Port: ${port}`);
