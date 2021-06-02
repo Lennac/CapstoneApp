c8140d5b207ece40c8441f4b8429e12eb12ed3ed
joeblake-tjx <83655435+joeblake-tjx@users.noreply.github.com>
2021-06-02 15:55:44 -0400

    Update app.js with ordersRouter

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const port = 3000;
 
 const indexRouter = require('./routes/index');
 const productsRouter = require('./routes/products');
-
+const ordersRouter = require('./routes/orders');
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on Port: ${port}`);
