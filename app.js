089a6765b178087592f59a6fe24d8f37fb8ef111
Andrew Cannell <and06571@tjx.com>
2021-06-02 10:35:28 -0400

    Update file structure and pathing

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const indexRouter = require('./routes/index');
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on Port: ${port}`);
 });
+
+module.exports = app;
