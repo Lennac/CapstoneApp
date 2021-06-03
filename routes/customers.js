b6957dd6763c6f3c6f1b041ec04aa93634648df6
nicolebryant <nicolee.bryant17@gmail.com>
2021-06-03 10:23:57 -0400

    Update customers route

--- routes/customers.js
+++ routes/customers.js
@@ -5,25 +5,19 @@ customersRouter.route('/')
     .get((req, res) => {
         res.send('Request: GET /customers');
     })
-    
-    .put((req, res) => {
-        
-        res.send('Posted')
+    .post((req, res) => {
+        res.send(`Request: POST /customers/${req.params['id']}`);
     });
-
 
     customersRouter.route('/:id')
     .get((req, res) => {
-        res.send('Request: GET /customers/'+req.params['id']);
-    })
-    
+        res.send(`Request: GET /customers/${req.params['id']}`);
+    })  
     .delete((req, res) => {
-        res.send('Request: DELETE '+req.params['id']); 
-    })
-    
+        res.send(`Request: DELETE /customers/${req.params['id']}`); 
+    }) 
     .put((req, res) => {
-        let customerId = Number(req.params.id); // is this a number in the db
-        res.send('Request: POST '+req.params['id']);
+        res.send(`Request: PUT /customers/${req.params['id']}`);
     });
 
 module.exports = customersRouter;
